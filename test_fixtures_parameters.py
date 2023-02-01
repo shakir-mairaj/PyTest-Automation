e01bf67367a64fbf8a69c325e4eaa9273b1a3e72
shakir-mairaj <shakir.mufti@gmail.com>
2023-02-02 00:35:22 +0530

    Fixtures with parameters

---
+++ test_fixtures_parameters.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(params=["chrome", "firefox"], scope='class')
+def init_driver(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome("executable_path=C://chromedriver.exe")
+    if request.param == "firefox":
+        driver = webdriver.Firefox()
+    request.cls.driver = driver
+    yield
+    driver.close()
+
+
+@pytest.mark.usefixtures("init_driver")
+class BaseTest:
+    pass
+
+
+class Test_Google(BaseTest):
+    def test_google_title(self):
+        self.driver.get("https://www.google.com")
+        assert self.driver.title == "Google"
